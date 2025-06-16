faeae02d3a0a461fdbe859adb8466e4fda854f01
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2025-06-15 20:10:35 -0500

    Update Booking.jsx

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import DatePicker from "react-datepicker";
+// import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Booking.scss";
 
